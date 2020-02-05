011d4311c7ef89a9079ea3ae8fd6cc4bb02471f9
greanyd4343 <greanyd4343@CA-SU-F106-01.sduhsd.lan>
2020-02-05 11:46:10 -0800

    Distance Lab progress

--- APCS-A-2020/src/Unit3/DistanceRunner.java
+++ APCS-A-2020/src/Unit3/DistanceRunner.java
@@ -7,7 +7,10 @@ package Unit3;
 //Class -
 //Lab  -
 
-import java.util.Scanner; 
+import java.util.Scanner;
+
+import Unit2.Line;
+
 import static java.lang.System.*;
 import static java.lang.Math.*;
 
@@ -15,7 +18,22 @@ public class DistanceRunner
 {
 	public static void main( String[] args )
 	{
-		//add test cases	
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Enter first x-coordinate :: ");
+		int x1 = keyboard.nextInt();
+		System.out.print("Enter first y-coordinate :: ");
+		int y1 = keyboard.nextInt();
+		System.out.print("Enter second x-coordinate :: ");
+		int x2 = keyboard.nextInt();
+		System.out.print("Enter second y-coordinate :: ");
+		int y2 = keyboard.nextInt();
+		
+		Distance test = new Distance(x1, y1, x2, y2);
+		test.setCoordinates(x1, y1, x2, y2);
+		test.calcDistance();
+		test.getDistance();
+		System.out.println(test);
+		
 			
 	}
 }
