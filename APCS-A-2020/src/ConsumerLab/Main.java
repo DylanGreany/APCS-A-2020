049c213646f4d781410665e5747c2ea975626099
greanyd4343 <greanyd4343@CA-SU-F106-01.sduhsd.lan>
2020-02-26 11:56:31 -0800

    unit 9

--- APCS-A-2020/src/ConsumerLab/Main.java
+++ APCS-A-2020/src/ConsumerLab/Main.java
@@ -6,16 +6,7 @@ class Main {
 	public static void main(String[] args) {
 		// test reading from files
 		// read in the positive adjectives in postiveAdjectives.txt
-		try {
-			Scanner input = new Scanner(new File("APCS-A-2020/src/ConsumerLab/positiveAdjectives.txt"));
-			while (input.hasNextLine()) {
-				String temp = input.nextLine().trim();
-				System.out.println(temp);
-			}
-			input.close();
-		} catch (Exception e) {
-			System.out.println("Error reading or parsing postitiveAdjectives.txt\n" + e);
-		}
+		
 		//test sentval
 		System.out.print("Sentiment Value Tests:");
 		System.out.println(Review.sentimentVal("mellow"));
@@ -24,17 +15,17 @@ class Main {
 		System.out.println(Review.sentimentVal("catastrophic"));
 		System.out.println();
 		//test totalSentiment
-		System.out.println("Total Simple Sentiment Value: " + Review.totalSentiment("APCS-A-2020/src/ConsumerLab/simpleReview.txt"));
-		System.out.println("Total 26 West Sentiment Value: " + Review.totalSentiment("APCS-A-2020/src/ConsumerLab/26WestReview.txt"));
+		System.out.println("Total Simple Sentiment Value: " + Review.totalSentiment("src/ConsumerLab/simpleReview.txt"));
+		System.out.println("Total 26 West Sentiment Value: " + Review.totalSentiment("src/ConsumerLab/26WestReview.txt"));
 		System.out.println();
 		
 		//stars
-		System.out.println("Simple Star Rating: " + Review.starRating("APCS-A-2020/src/ConsumerLab/simpleReview.txt"));
-		System.out.println("26 West Star Rating: " + Review.starRating("APCS-A-2020/src/ConsumerLab/26WestReview.txt"));
+		System.out.println("Simple Star Rating: " + Review.starRating("src/ConsumerLab/simpleReview.txt"));
+		System.out.println("26 West Star Rating: " + Review.starRating("src/ConsumerLab/26WestReview.txt"));
 		System.out.println();
 		
 		//fake
-		System.out.println("Fake Simple Review: " + Review.fakeReview("APCS-A-2020/src/ConsumerLab/simpleReview.txt", false));
+		System.out.println("Fake Simple Review: " + Review.fakeReview("src/ConsumerLab/simpleReview.txt", false));
 		
 	}	
 }
---
+++ APCS-A-2020/src/Unit9/ListSumFirst.java
@@ -0,0 +1,27 @@
+package Unit9;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListSumFirst
+{
+	public static int go(List<Integer> ray)
+	{
+		int first = ray.get(0);
+		int total = 0;
+		for (int i = 0; i < ray.size(); i++) {
+			if(ray.get(i) > first) {
+				total += ray.get(i);
+			}
+		}
+		if (total == 0 || ray.size() < 1)
+			return -1;
+		else
+			return total;
+		
+	}
+}
